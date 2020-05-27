bac853b9e6d8ad9b146518247d79076bbf04606d
Karl Larson <kllarson@hotmail.com>
2020-05-27 10:48:42 -0500

    yep added it

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?><br>
+Your password is: <?php echo $_GET["pword"]; ?><br>
+Your hidden field is: <?php echo $_GET["hfield"]; ?>
+</body>
+</html>
